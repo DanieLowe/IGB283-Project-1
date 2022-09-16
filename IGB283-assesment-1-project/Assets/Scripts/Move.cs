1edbdb06115bdf666284cfbce5f5894883e2af9b
DanieLowe <104814678+DanieLowe@users.noreply.github.com>
2022-09-16 15:37:46 +1000

    1

--- IGB283-assesment-1-project/Assets/Scripts/Move.cs
+++ IGB283-assesment-1-project/Assets/Scripts/Move.cs
@@ -9,6 +9,7 @@ public class Move : MonoBehaviour
 
     public float angle;
     public Vector3 movement = new Vector3(1.0f,0f,0f);
+    public Vector3 oldPos;
     void Start()
     {
         
@@ -17,17 +18,18 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        oldPos = this.gameObject.GetComponent<Renderer>().bounds.extents;
         Mesh mesh = GetComponent<MeshFilter>().mesh;
 
         Vector3[] vertices = mesh.vertices;
         Vector3 currentPos = this.gameObject.transform.position;
 
         IGB283Transform M = iGB283Transform.Translate(movement);
-        IGB283Transform Td1 = iGB283Transform.Translate(-transform.GetComponent<Renderer>().bounds.center);
+        IGB283Transform Td1 = iGB283Transform.Translate(-oldPos);
         IGB283Transform Rs = iGB283Transform.Rotate(angle * Time.deltaTime);
-        IGB283Transform Td2 = iGB283Transform.Translate(transform.GetComponent<Renderer>().bounds.center);
+        IGB283Transform Td2 = iGB283Transform.Translate(oldPos);
 
-         M = M * Td2 * Rs * Td1;
+         M =  Td2 * M * Rs * Td1;
 
         for (int i = 0; i < vertices.Length; i++)
         {
