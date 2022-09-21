fbd7016b72f307185c77ebb8377a69b7035ff6e2
DanieLowe <104814678+DanieLowe@users.noreply.github.com>
2022-09-21 15:40:02 +1000

    final 3d

--- IGB283-assesment-1-project/Assets/Scripts/square.cs
+++ IGB283-assesment-1-project/Assets/Scripts/square.cs
@@ -6,7 +6,10 @@ public class square : MonoBehaviour
 {
     public Material material;
     public GameObject selectedObject;
-    Vector3 offset;
+    
+    
+
+    IGB283Transform iGB283Transform = new IGB283Transform();
     // Start is called before the first frame update
     void Start()
     {
@@ -48,25 +51,9 @@ public class square : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
-        {
-            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
-            if (targetObject)
-            {
-                selectedObject = targetObject.transform.gameObject;
-                offset = selectedObject.transform.position - mousePosition;
-            }
-        }
-        if (selectedObject)
-        {
-            selectedObject.transform.position = mousePosition + offset;
-        }
-        if (Input.GetMouseButtonUp(0) && selectedObject)
-        {
-            selectedObject = null;
-        }
+        
     }
+    
 }
     
 
